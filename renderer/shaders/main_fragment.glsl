ad48fea10afe36ffe7d5ecb3ad3a73c09eb64810
Jack Bennett <jackbennett0708@gmail.com>
2023-05-25 22:19:06 +0100

    GLSL formatting

--- renderer/shaders/main_fragment.glsl
+++ renderer/shaders/main_fragment.glsl
@@ -2,10 +2,10 @@
 
 #pragma shader_stage(fragment)
 
-layout(location = 0) in vec3 in_vertex_colour;
+layout(location = 0) in vec3    i_VERTEX_COLOUR;
 
-layout(location = 0) out vec4 out_colour;
+layout(location = 0) out vec4   o_COLOUR;
 
 void main() {
-    out_colour = vec4(in_vertex_colour, 1.0);
+    o_COLOUR = vec4(i_VERTEX_COLOUR, 1.0);
 }
--- renderer/shaders/main_vertex.glsl
+++ renderer/shaders/main_vertex.glsl
@@ -2,13 +2,13 @@
 
 #pragma shader_stage(vertex)
 
-layout(location = 0) in vec2 in_position;
-layout(location = 1) in vec3 in_colour;
+layout(location = 0) in vec2    i_POSITION;
+layout(location = 1) in vec3    i_COLOUR;
 
-layout(location = 0) out vec3 out_vertex_colour;
+layout(location = 0) out vec3   o_VERTEX_COLOUR;
 
 void main() {
-    gl_Position = vec4(in_position, 0.0, 1.0);
+    gl_Position = vec4(i_POSITION, 0.0, 1.0);
 
-    out_vertex_colour = in_colour;
+    o_VERTEX_COLOUR = i_COLOUR;
 }
