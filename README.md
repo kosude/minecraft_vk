2f8dd20a3f69a2e0f8fab057de716db5ff4b8031
Jack Bennett <jackbennett0708@gmail.com>
2023-12-30 19:56:35 +0000

    Initial commit; project set-up.

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Minecraft clone using Vulkan written in C++
---
+++ src/game/main.cpp
@@ -0,0 +1,20 @@
+/*
+ *   Copyright (c) 2023 Jack Bennett.
+ *   All Rights Reserved.
+ *
+ *   See the LICENCE file for more information.
+ */
+
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+#include <volk.h>
+#include <glm/glm.hpp>
+#include <cio.h>
+#include <cioenum.h>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello minecraft_vk" << std::endl;
+
+    return 0;
+}
