b14ce96097f1375ae9463ecc1e713c91c1698a8a
Jack Bennett <jackbennett0708@gmail.com>
2024-01-09 23:23:07 +0000

    Add grass block and (badly programmed + rushed) monkey

--- game/game.cpp
+++ game/game.cpp
@@ -38,30 +38,48 @@ namespace mcvk::Game {
         ResourceMgr::ModelResource mdl;
         _resources.Load("cube.model", mdl);
         auto model = Renderer::Model::CreateFromResource(mdl);
+        ResourceMgr::ModelResource monkey_mdl;
+        _resources.Load("monkey.model", monkey_mdl);
+        auto monkey_model = Renderer::Model::CreateFromResource(monkey_mdl);
 
         Renderer::VertexBuffer vbo{_renderer.GetDevice(), model.GetVertexDataSize()};
         vbo.Map();
         vbo.Write(model.GetVertexDataPtr());
         vbo.Unmap();
-
         Renderer::IndexBuffer ibo{_renderer.GetDevice(), model.GetIndexDataSize(), Renderer::Model::GetIndexType()};
         ibo.Map();
         ibo.Write(model.GetIndexDataPtr());
         ibo.Unmap();
 
+        Renderer::VertexBuffer monkey_vbo{_renderer.GetDevice(), monkey_model.GetVertexDataSize()};
+        monkey_vbo.Map();
+        monkey_vbo.Write(monkey_model.GetVertexDataPtr());
+        monkey_vbo.Unmap();
+        Renderer::IndexBuffer monkey_ibo{_renderer.GetDevice(), monkey_model.GetIndexDataSize(), Renderer::Model::GetIndexType()};
+        monkey_ibo.Map();
+        monkey_ibo.Write(monkey_model.GetIndexDataPtr());
+        monkey_ibo.Unmap();
+
         Renderer::UniformBuffer ubo_global{_renderer, sizeof(GlobalUniformData)};
-        Renderer::UniformBuffer ubo_model{_renderer, sizeof(ModelUniformData) * 1}; // per-instance data
+        Renderer::UniformBuffer ubo_model{_renderer, sizeof(ModelUniformData) * 2}; // per-instance data
 
         ResourceMgr::MaterialResource mat;
-        _resources.Load("uvguide.material", mat);
+        _resources.Load("grass_block.material", mat);
         auto grass_img_config = Renderer::Image::Config::Defaults(
             {(uint32_t) mat.colourmap->width, (uint32_t) mat.colourmap->height}, VK_FORMAT_R8G8B8A8_SRGB);
         Renderer::Image grass_img{_renderer.GetDevice(), grass_img_config, *mat.colourmap};
 
+        ResourceMgr::MaterialResource monkey_mat;
+        _resources.Load("monkey.material", monkey_mat);
+        auto monkey_img_config = Renderer::Image::Config::Defaults(
+            {(uint32_t) monkey_mat.colourmap->width, (uint32_t) monkey_mat.colourmap->height}, VK_FORMAT_R8G8B8A8_SRGB);
+        Renderer::Image monkey_img{_renderer.GetDevice(), monkey_img_config, *monkey_mat.colourmap};
+
         VkDescriptorSetLayout dset_layout = Renderer::DescriptorSetLayoutBuilder::New()
             .AddBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT)
             .AddBinding(1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1, VK_SHADER_STAGE_VERTEX_BIT)
-            .AddBinding(2, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT)
+            .AddBinding(2, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT) // TODO find out how to properly do multiple samplers
+            .AddBinding(3, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT)
             .Build(_renderer.GetDevice());
 
         _renderer.BuildPipelines({ dset_layout });
@@ -78,16 +96,18 @@ namespace mcvk::Game {
             .AddWriteBuffer(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, ubo_global)
             .AddWriteBuffer(1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, ubo_model, 0, sizeof(ModelUniformData))
             .AddWriteImage(2, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, grass_img)
+            .AddWriteImage(3, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, monkey_img)
             .UpdateSet(_renderer.GetDevice(), dset);
+
+        const Renderer::GraphicsPipeline &g_simple = _renderer.Pipelines().GraphicsByName("g_simple");
+        const Renderer::GraphicsPipeline &g_wireframe = _renderer.Pipelines().GraphicsByName("g_wireframe");
+        const Renderer::GraphicsPipeline &g_monkey = _renderer.Pipelines().GraphicsByName("g_monkey");
 
         Utils::Info("Entering main loop...");
         while (true) {
             if (!_window.Update()) {
                 break;
             }
-
-            const Renderer::GraphicsPipeline &g_simple = _renderer.Pipelines().GraphicsByName("g_simple");
-            const Renderer::GraphicsPipeline &g_wireframe = _renderer.Pipelines().GraphicsByName("g_wireframe");
 
             {
                 GlobalUniformData d;
@@ -96,9 +116,11 @@ namespace mcvk::Game {
                 ubo_global.Write(&d);
             }
             {
-                ModelUniformData d[1];
+                ModelUniformData d[2];
                 d[0].transform = glm::rotate(glm::mat4{1.0f}, (float) glm::radians(std::fmod(glfwGetTime() * 100, 360)), glm::vec3{0, 1, 0});
-                // d[0].transform = glm::mat4{1.0f};
+                d[1].transform = glm::scale(glm::rotate(glm::translate(glm::mat4{1.0f}, glm::vec3{0, -0.9f, 0}),
+                    (float) glm::radians(std::fmod(glfwGetTime() * 100, 360)), glm::vec3{0, 1, 0}), glm::vec3{0.25f});
+
                 ubo_model.Write(&d);
             }
 
@@ -107,12 +129,17 @@ namespace mcvk::Game {
 
                 drawbuf->UpdateViewportAndScissor();
 
+                drawbuf->BindPipeline(g_simple);
                 drawbuf->BindVertexBuffer(vbo);
                 drawbuf->BindIndexBuffer(ibo);
-
-                drawbuf->BindPipeline(g_simple);
                 drawbuf->BindDescriptorSets(g_simple, { dset }, { 0 });
                 drawbuf->DrawIndexed(model.indices.size());
+
+                drawbuf->BindPipeline(g_monkey);
+                drawbuf->BindVertexBuffer(monkey_vbo);
+                drawbuf->BindIndexBuffer(monkey_ibo);
+                drawbuf->BindDescriptorSets(g_simple, { dset }, { sizeof(ModelUniformData) });
+                drawbuf->DrawIndexed(monkey_model.indices.size());
 
                 drawbuf->EndRenderPass();
                 drawbuf->End();
---
+++ resources/shaders/glsl/monkey.frag
@@ -0,0 +1,13 @@
+#version 450
+#pragma shader_stage(fragment)
+
+layout(location = 0) in vec3 i_VERTEX_COLOUR;
+layout(location = 1) in vec2 i_UV;
+
+layout(location = 0) out vec4 o_COLOUR;
+
+layout(set = 0, binding = 3) uniform sampler2D u_SAMPLER;
+
+void main() {
+    o_COLOUR = texture(u_SAMPLER, i_UV);
+}
